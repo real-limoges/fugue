0a6493fe384a7a904dc790d44a2938fdb0270d8b
Real Limoges <b.real.limoges@gmail.com>
2026-05-08 19:44:55 -0700

    rip out the blog

--- lib/fugue_web/controllers/page_controller.ex
+++ lib/fugue_web/controllers/page_controller.ex
@@ -3,7 +3,6 @@ defmodule FugueWeb.PageController do
   use FugueWeb, :controller
 
   def home(conn, _params) do
-    latest_post = Fugue.Blog.list_posts() |> List.first()
-    render(conn, :home, latest_post: latest_post)
+    render(conn, :home)
   end
 end
--- test/support/fixtures/blog/003-draft.md
+++
@@ -1,9 +0,0 @@
----
-title: Draft Post
-date: 2026-03-01
-tags: meta
-summary: A draft fixture; should not appear in list_posts.
-draft: true
----
-
-If you can read this through the public API, draft filtering is broken.
